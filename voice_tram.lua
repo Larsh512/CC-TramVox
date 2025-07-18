eb6a322a4586806b6b65840a47c8f883922673ce
Lars Hiemstra <154842522+Larsh512@users.noreply.github.com>
2025-07-18 16:11:33 +0200

    Tram Voice. Still requires some changes.
    Simple test

---
+++ voice_tram.lua
@@ -0,0 +1,56 @@
+--[[
+    VOX API vX.X.XX (indev)
+    (c) 2021 Peekofwar
+
+    This is the sound index for the VOX voice.
+    The root folder is determined by the filename of the sound index file (this file)
+    
+
+    -- The voice lines will be indexed something like this:
+    {
+        word = {
+            path = "word", -- path to the sound file relative to the root folder
+            length = 0.8, -- Length of the sound (not counting reverb) in seconds
+        },
+    }
+]]
+{
+    __voiceRoot = "aci.vox.voice_tram",
+    beepboop = {
+        path = "beepboop",
+        length = 0.850,
+    },
+    bizwarn = {
+        path = "bizwarn",
+        length = 0.454,
+    },
+    buzwarn = {
+        path = "buzwarn",
+        length = 0.250,
+    },
+    deeoo = {
+        path = "deeoo",
+        length = 0.600,
+    },
+    doop = {
+        path = "doop",
+        length = 0.700,
+    },
+    woop = {
+        path = "woop",
+        length = 0.401,
+    },
+    bm = {
+        path = "bm",
+        length = 0.401,
+    },
+    wel1 = {
+        path = "wel1",
+        length = 0.401,
+    },
+    wel2 = {
+        path = "wel2",
+        length = 0.401,
+    },
+}
+
